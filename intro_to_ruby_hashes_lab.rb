86c7584014a6e39c93da1658191726859f9e6267
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-02-19 23:42:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,12 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {}
+	  :railroads => {
+	    :pieces => 4,
+	    :rent_in_dollars => {}
+	  }
 	}
-  monopoly[:railroads][:pieces] = 4
+  
 end
 
 def monopoly_with_third_tier
